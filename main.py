53240526f778105dd6d42f5b08f6a9454bf5c3f3
kailongli27 <55965406+kailongli27@users.noreply.github.com>
2020-10-27 00:51:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import pygame
+import sys
 
-pygame.init() # initialize all imported pygame modules
-screen = pygame.display.set_mode((576, 1024)) # initialize a display surface of 576 px x 1024 px
+pygame.init()  # initialize all imported pygame modules
+screen = pygame.display.set_mode(size=(576, 1024))  # initialize a display surface of 576 px x 1024 px
+
+# game loop
+while True:
+    for event in pygame.event.get():  # get events from the queue (e.g. user moving mouse, timer ending, etc.)
+        if event.type == pygame.QUIT:
+            pygame.quit()  # uninitialize all pygame modules
+            sys.exit()
+    pygame.display.update()
