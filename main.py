8935954bc99efbfec1955c852e89b21a370b012d
kailongli27 <55965406+kailongli27@users.noreply.github.com>
2020-10-29 01:08:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import pygame
 import sys
 
+
+def draw_floor():
+    screen.blit(floor_surface, (floor_x_pos, 656))  # put this floor at the left side of the screen
+    screen.blit(floor_surface, (floor_x_pos + 576, 656))  # put another floor at the right side of the screen
+
+
 pygame.init()  # initialize all imported pygame modules
-screen = pygame.display.set_mode(size=(576, 1024))  # initialize a display surface of 576 px x 1024 px
+screen = pygame.display.set_mode(size=(576, 780))  # initialize a display surface of 576 px x 780 px
+clock = pygame.time.Clock()  # create an object to help track time
+
+# import images
+bg_surface = pygame.image.load('assets/background-day.png').convert()
+bg_surface = pygame.transform.scale2x(bg_surface)  # double the size of the surface
+
+floor_surface = pygame.image.load('assets/base.png').convert()
+floor_surface = pygame.transform.scale2x(floor_surface)
+floor_x_pos = 0
 
 # game loop
 while True:
@@ -10,4 +25,11 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()  # uninitialize all pygame modules
             sys.exit()
+    screen.blit(bg_surface, (0, 0))  # position the background image at the origin point of the screen
+    floor_x_pos -= 1  # make the floor move to the left
+    draw_floor()
+    if floor_x_pos <= -576:  # ensure a continuously moving floor
+        floor_x_pos = 0  # reset to the left side of the screen
+
     pygame.display.update()
+    clock.tick(120)  # limit frame rate to 120 frames per second
